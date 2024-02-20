1d10b9593de1aa24c92bad1a1d0de2c735cd84af
Ravinder <ravinder.jnv2019@gmail.com>
2024-02-20 20:33:41 +0530

    third commit: Add gitignore and update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const contactContent = "Thank you for reaching out to us! We're delighted to assist you in any way we can. Whether you have questions, suggestions, or feedback, we're here to listen.";
 
 const app = express();
 
@@ -34,7 +34,6 @@ app.get("/",function(re,res){
 
   Post.find({}).then(function(posts){
     res.render("home",{homeContent : homeStartingContent, newPosts : posts});
-    console.log(posts);
   });
 });
 
@@ -52,7 +51,7 @@ app.get("/compose",function(req,res){
 
 app.post("/compose",function(req,res){
   const post = new Post({
-    title : req.body.input1,
+    title : low.capitalize(req.body.input1),
     content : req.body.textArea
   });
   post.save();
